f0d97ac83a5b3e6aa76b29dda1b4dabd7e1037ac
박성민 <44596433+dnjsals45@users.noreply.github.com>
2024-05-21 13:09:34 +0900

    [level 1] Title: 최소직사각형, Time: 2.31 ms, Memory: 96.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] sizes) {
+        int rowMax = 0;
+        int colMax = 0;
+        
+       for (int[] size : sizes) {
+           rowMax = Math.max(rowMax, Math.max(size[0], size[1]));
+           colMax = Math.max(colMax, Math.min(size[0], size[1]));
+       } 
+        
+        return rowMax * colMax;
+    }
+}
