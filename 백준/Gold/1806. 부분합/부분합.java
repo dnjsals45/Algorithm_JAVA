9179041328a3907761cd0369d0c39d82a701ec3c
박성민 <44596433+dnjsals45@users.noreply.github.com>
2024-10-23 17:18:56 +0900

    [Gold IV] Title: 부분합, Time: 248 ms, Memory: 25388 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -13,19 +13,21 @@ public class Main {
         int S = Integer.parseInt(st.nextToken());
 
         String[] strings = br.readLine().split(" ");
-        int[] arr = new int[N];
+        int[] arr = new int[N + 1];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(strings[i]);
         }
 
-        for (int i = 0; i < N; i++) {
-            int sum = 0;
-            for (int j = i; j < N; j++) {
-                sum += arr[j];
-                if (sum >= S) {
-                    min = Math.min(min, j - i + 1);
-                    break;
-                }
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+
+        while (end <= N) {
+            if (sum >= S) {
+                min = Math.min(min, end - start);
+                sum -= arr[start++];
+            } else {
+                sum += arr[end++];
             }
         }
 
