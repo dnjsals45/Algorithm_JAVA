cb83705f3b5b45e194153f024cfee4917e0a4804
박성민 <44596433+dnjsals45@users.noreply.github.com>
2025-03-14 15:04:28 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,18 +1,18 @@
-// yellow 가장 적은 차이의 곱 (ex 24면 2 * 12가 아니라 4 * 6) 에서, 가로 세로 +2 씩 하면 정답(가로가 더 큰거)
 import java.util.*;
 
 class Solution {
-    public int[] solution(int brown, int yellow) {
-
-        for (int i = 1; i <= Math.sqrt(yellow); i++) {
-            if (yellow % i == 0) {
-                int j = yellow / i;
-                if ((i + 2) * (j + 2) == brown + yellow) {
-                    return new int[]{j + 2, i + 2};
+    public int[] solution(int brown, int yellow) {        
+        int sum = brown + yellow;
+        
+        for (int y = 1; y <= sum; y++) {
+            if (sum % y == 0) {
+                int x = sum / y;
+                
+                if ((x - 2) * (y - 2) == yellow) {
+                    return new int[]{x, y};
                 }
             }
         }
-        
-        return new int[]{};
+        return null;
     }
 }
