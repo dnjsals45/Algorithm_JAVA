c6224707d818725c42e4a526530cc6f6b5726853
박성민 <44596433+dnjsals45@users.noreply.github.com>
2025-03-20 17:11:57 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,14 +3,14 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-        Map<String, Integer> comb = new HashMap<>();
+        Map<String, Integer> clothMap = new HashMap<>();
         
         for (String[] cloth : clothes) {
-            comb.put(cloth[1], comb.getOrDefault(cloth[1], 0) + 1);
+            clothMap.put(cloth[1], clothMap.getOrDefault(cloth[1], 0) + 1);
         }
         
-        for (int cnt : comb.values()) {
-            answer *= (cnt + 1);
+        for (String key : clothMap.keySet()) {
+            answer *= (clothMap.get(key) + 1);
         }
         
         return answer - 1;
