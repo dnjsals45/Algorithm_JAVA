8be798656ea3f118ad39ec907f43ce9825ff9478
박성민 <44596433+dnjsals45@users.noreply.github.com>
2025-03-20 18:01:14 +0900

    [level 3] Title: 베스트앨범, Time: 9.21 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,40 +1,43 @@
+// 속한노래 재생순 -> 장르 재생순 -> 고유번호순
 import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        ArrayList<Integer> answer = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();
+        Map<String, Integer> contain = new HashMap<>();
+        Map<String, Map<Integer, Integer>> genreMap = new HashMap<>();
         
-        // 장르 별 노래 재생 총 횟수
-        HashMap<String, Integer> count = new HashMap<>();
-        // 장르 별 개별 재생 횟수
-        HashMap<String, HashMap<Integer, Integer>> each = new HashMap<>();
         for (int i = 0; i < genres.length; i++) {
-            if (!count.containsKey(genres[i])) {
-                HashMap<Integer, Integer> data = new HashMap<>();
-                data.put(i, plays[i]);
-                count.put(genres[i], plays[i]);
-                each.put(genres[i], data);
-            } else {
-                count.put(genres[i], count.get(genres[i]) + plays[i]);
-                each.get(genres[i]).put(i, plays[i]);
-            }
-        }
-        
-        List<String> keySet = new ArrayList(count.keySet());
-        Collections.sort(keySet, (s1, s2) -> count.get(s2) - count.get(s1));
-        
-        for (String key : keySet) {
-            HashMap<Integer, Integer> data = each.get(key);
-            List<Integer> eachKey = new ArrayList(data.keySet());
-            
-            Collections.sort(eachKey, (s1, s2) -> data.get(s2) - data.get(s1));
-            
-            answer.add(eachKey.get(0));
-            if (eachKey.size() > 1) {
-                answer.add(eachKey.get(1));
-            }
-        }
+            contain.put(genres[i], contain.getOrDefault(genres[i], 0) + plays[i]);
 
+            if (!genreMap.containsKey(genres[i])) {
+               genreMap.put(genres[i], new HashMap<>());
+            }
+            
+            genreMap.get(genres[i]).put(i, plays[i]);
+        }
+        
+        List<String> containKeySet = new ArrayList<>(contain.keySet());
+        
+        containKeySet.sort((o1, o2) -> contain.get(o2).compareTo(contain.get(o1)));
+        
+        for (String key : containKeySet) {
+            Map<Integer, Integer> data = genreMap.get(key);
+            List<Integer> dataKeySet = new ArrayList<>(data.keySet());
+            
+            dataKeySet.sort((o1, o2) -> {
+                if (!data.get(o1).equals(data.get(o2))) {
+                    return data.get(o2) - data.get(o1);
+                }
+                return o1 - o2;
+            });
+            
+            answer.add(dataKeySet.get(0));
+            if (dataKeySet.size() >= 2) {
+                answer.add(dataKeySet.get(1));
+            }
+        }
+        
         return answer.stream().mapToInt(i -> i).toArray();
     }
 }
