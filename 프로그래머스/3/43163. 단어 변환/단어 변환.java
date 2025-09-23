93275e0bce7cf6580bb573ad57345ff3944dcaf4
박성민 <44596433+dnjsals45@users.noreply.github.com>
2025-09-23 10:35:02 +0900

    [level 3] Title: 단어 변환, Time: 0.37 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,57 +2,50 @@ import java.util.*;
 
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        boolean possible = false;
-        
-        for (String word : words) {
-            if (word.equals(target)) {
-                possible = true;
-                break;
-            }
-        }
-        
-        if (!possible) return 0;
+        // words 배열 내에 target이 없을 경우 early return
+        if (!isPossible(words, target)) return 0;
         
         boolean[] visited = new boolean[words.length];
-        
-        Queue<Word> q = new LinkedList<>();
-        q.add(new Word(begin, 0));
+        Queue<String> q = new LinkedList<>();
+        q.add(begin);
+        int answer = 0;
         
         while (!q.isEmpty()) {
-            Word cur = q.poll();
+            int size = q.size();
+            answer++;
             
-            if (cur.word.equals(target)) return cur.depth;
-            
-            for (int i = 0; i < words.length; i++) {
-                if (!visited[i] && canConvert(cur.word, words[i])) {
-                    visited[i] = true;
-                    q.add(new Word(words[i], cur.depth + 1));
+            for (int s = 0; s < size; s++) {
+                String cur = q.poll();
+
+                for (int i = 0; i < words.length; i++) {
+                    if (!visited[i] && canConvert(cur, words[i])) {
+                        // 다음 번 변환 값이 target과 같다면 return
+                        if (words[i].equals(target)) return answer;
+                        visited[i] = true;
+                        q.add(words[i]);
+                    }
                 }
             }
         }
         
-        int answer = 0;
         return answer;
     }
     
+    // words 배열 내에 해당하는 단어 존재 여부 체크
+    private boolean isPossible(String[] words, String target) {
+        for (String word: words) {
+            if (word.equals(target)) return true;
+        }
+        return false;
+    }
+    
+    // 한 글자만 틀린지 체크
     private boolean canConvert(String word, String target) {
         int diff = 0;
-        
         for (int i = 0; i < word.length(); i++) {
             if (word.charAt(i) != target.charAt(i)) diff++;
         }
         
-        return diff == 1;
-    }
-    
-    public class Word {
-        String word;
-        int depth;
-        
-        public Word(String w, int d) {
-            this.word = w;
-            this.depth = d;
-        }
-        
+        return diff == 1 ? true : false;
     }
 }
