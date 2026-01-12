88564adbfbb1f72712305156b9a0f60824198a10
박성민 <44596433+dnjsals45@users.noreply.github.com>
2026-01-12 16:10:58 +0900

    [level 2] Title: 주식가격, Time: 16.24 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,18 +1,14 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
         
-        for (int i = 0; i < prices.length; i++) {
-            int term = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            int cnt = 0;
             for (int j = i + 1; j < prices.length; j++) {
-                term++;
-                if (prices[i] > prices[j]) {
-                    break;
-                }
+                cnt++;
+                if (prices[i] > prices[j]) break;
             }
-            answer[i] = term;
+            answer[i] = cnt;
         }
         
         return answer;
