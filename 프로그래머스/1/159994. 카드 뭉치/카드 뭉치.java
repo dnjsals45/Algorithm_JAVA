36afb60d44a02476885e48ca04932409b9bbde30
박성민 <44596433+dnjsals45@users.noreply.github.com>
2026-01-13 15:50:43 +0900

    [level 1] Title: 카드 뭉치, Time: 0.19 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        Queue<String> q1 = new LinkedList<>();
+        Queue<String> q2 = new LinkedList<>();
+        
+        for (String s : cards1) {
+            q1.add(s);
+        }
+        
+        for (String s : cards2) {
+            q2.add(s);
+        }
+        
+        for (String s : goal) {
+            if (!q1.isEmpty() && q1.peek().equals(s)) {
+                q1.poll();
+                continue;
+            }
+            
+            if (!q2.isEmpty() && q2.peek().equals(s)) {
+                q2.poll();
+                continue;
+            }
+            
+            return "No";
+        }
+        
+        return "Yes";
+    }
+}
