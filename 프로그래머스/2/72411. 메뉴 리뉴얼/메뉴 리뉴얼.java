289820d6baa72e69a574dca4f8a326390bdaa5de
박성민 <44596433+dnjsals45@users.noreply.github.com>
2026-01-14 15:39:40 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 22.22 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,57 +1,52 @@
 import java.util.*;
 
 class Solution {
+    public static Map<Integer, Map<String, Integer>> map;
     
     public String[] solution(String[] orders, int[] course) {
-        Map<Integer, Map<String, Integer>> map = new HashMap<>();
+        map = new HashMap<>();
+        
+        for (int c : course) {
+            map.put(c, new HashMap<>());
+        }
+        
+        for (String order : orders) {
+            char[] arr = order.toCharArray();
+            Arrays.sort(arr);
+            comb(arr, "", 0);
+        }
+        
         List<String> answer = new ArrayList<>();
-
-        for (int count : course) {
-            map.put(count, new HashMap<>());
-        }
         
-        for (int count : course) {
-            for (String order : orders) {
-                char[] arr = order.toCharArray();
-                Arrays.sort(arr);
-                combination(0, count, new String(arr), new StringBuilder(), map.get(count), 0);
+        for (Map<String, Integer> orderMap : map.values()) {
+            int max = 0;
+            
+            for (int cnt : orderMap.values()) {
+                max = Math.max(max, cnt);
             }
-        }
-        
-        for (int count : course) {
-            int max = 2;
-            List<String> list = new ArrayList<>();
-            for (String key : map.get(count).keySet()) {
-                int tmp = map.get(count).get(key);
-                if (tmp > 1) {
-                    if (tmp > max) {
-                        max = tmp;
-                        list.clear();
-                        list.add(key);
-                    } else if (tmp == max) {
-                        list.add(key);
+            
+            if (max > 1) {
+                for (Map.Entry<String, Integer> entry : orderMap.entrySet()) {
+                    if (entry.getValue() == max) {
+                        answer.add(entry.getKey());
                     }
                 }
             }
-            answer.addAll(list);
         }
         
         Collections.sort(answer);
         
-        return answer.stream().toArray(String[]::new);
+        return answer.toArray(new String[0]);
     }
     
-    public void combination(int depth, int count, String order, StringBuilder now, Map<String, Integer> map, int index) {
-        if (now.length() == count) {
-            map.put(now.toString(), map.getOrDefault(now.toString(), 0) + 1);
-            return;
+    public void comb(char[] arr, String now, int depth) {
+        if (map.containsKey(now.length())) {
+            Map<String, Integer> orderMap = map.get(now.length());
+            orderMap.put(now, orderMap.getOrDefault(now, 0) + 1);
         }
         
-        for (int i = index; i < order.length(); i++) {
-            now.append(order.charAt(i));
-            combination(depth + 1, count, order, now, map, i + 1);
-            now.deleteCharAt(now.length() - 1);
+        for (int i = depth; i < arr.length; i++) {
+            comb(arr, now + arr[i], i + 1);
         }
-        
     }
 }
