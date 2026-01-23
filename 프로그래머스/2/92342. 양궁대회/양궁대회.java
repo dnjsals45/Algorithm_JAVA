0eda2837290cd5e79f9a057d1053acac32779eb7
박성민 <44596433+dnjsals45@users.noreply.github.com>
2026-01-23 17:16:41 +0900

    [level 2] Title: 양궁대회, Time: 0.72 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,17 +1,15 @@
-import java.util.*;
-
 class Solution {
-    static int max = 0;
-    static int[] answer = {-1};
-    
+    private static int max;
+    private static int[] answer = {-1};
     public int[] solution(int n, int[] info) {
         int[] arr = new int[11];
-        dfs(0, n, info, arr);
+        
+        solve(0, n, info, arr);
         
         return answer;
     }
     
-    public void dfs(int idx, int arrows, int[] info, int[] arr) {
+    public void solve(int idx, int arrows, int[] info, int[] arr) {
         if (idx == 11) {
             if (arrows > 0) {
                 arr[10] = arrows;
@@ -32,25 +30,26 @@ class Solution {
             
             arr[10] = 0;
             return;
-        }
+        }        
         
         int need = info[idx] + 1;
         if (arrows >= need) {
             arr[idx] = need;
-            dfs(idx + 1, arrows - need, info, arr);
+            solve(idx + 1, arrows - need, info, arr);
             arr[idx] = 0;
         }
         
-        dfs(idx + 1, arrows, info, arr);
+        solve(idx + 1, arrows, info, arr);
     }
     
-    private int calculate(int[] a, int[] b) {
+    public int calculate(int[] a, int[] b) {
         int score = 0;
         
-        for (int i = 0; i < 11; i++) {
+        for (int i = 0; i < a.length; i++) {
             if (a[i] == 0 && b[i] == 0) {
                 continue;
             }
+        
             if (a[i] > b[i]) {
                 score += (10 - i);
             } else {
@@ -61,7 +60,7 @@ class Solution {
         return score;
     }
     
-    private boolean compareLowScore(int[] a, int[] b) {
+    public boolean compareLowScore(int[] a, int[] b) {
         for (int i = 10; i >= 0; i--) {
             if (a[i] > b[i]) {
                 return true;
